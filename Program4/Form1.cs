683a38558d6ee78c39ca8ef0f907cb04126a2c23
Joshua Williams <jbrockwilliams@gmail.com>
2022-09-22 10:51:26 -0500

    Front-End

--- Program4/Form1.cs
+++ Program4/Form1.cs
@@ -21,5 +21,25 @@ namespace Program4
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
