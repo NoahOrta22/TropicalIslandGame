e56d7c0abf2d1bff73d1028321046de6aed3468c
Joshua Williams <jbrockwilliams@gmail.com>
2022-09-23 13:42:16 -0500

    COLORS! More to come just a starrt

--- Program4/Game.Designer.cs
+++ Program4/Game.Designer.cs
@@ -53,7 +53,7 @@
             // 
             this.MapSizeButton.Location = new System.Drawing.Point(124, 55);
             this.MapSizeButton.Name = "MapSizeButton";
-            this.MapSizeButton.Size = new System.Drawing.Size(41, 23);
+            this.MapSizeButton.Size = new System.Drawing.Size(57, 23);
             this.MapSizeButton.TabIndex = 0;
             this.MapSizeButton.Text = "Enter";
             this.MapSizeButton.UseVisualStyleBackColor = true;
@@ -63,7 +63,7 @@
             // 
             this.MapRows.Location = new System.Drawing.Point(6, 55);
             this.MapRows.Name = "MapRows";
-            this.MapRows.Size = new System.Drawing.Size(34, 20);
+            this.MapRows.Size = new System.Drawing.Size(34, 22);
             this.MapRows.TabIndex = 3;
             // 
             // label1
@@ -71,7 +71,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(6, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.Size = new System.Drawing.Size(37, 14);
             this.label1.TabIndex = 4;
             this.label1.Text = "Rows";
             // 
@@ -79,7 +79,7 @@
             // 
             this.MapCol.Location = new System.Drawing.Point(64, 55);
             this.MapCol.Name = "MapCol";
-            this.MapCol.Size = new System.Drawing.Size(44, 20);
+            this.MapCol.Size = new System.Drawing.Size(44, 22);
             this.MapCol.TabIndex = 5;
             // 
             // label2
@@ -87,7 +87,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(61, 27);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.Size = new System.Drawing.Size(56, 14);
             this.label2.TabIndex = 6;
             this.label2.Text = "Columns";
             // 
@@ -98,6 +98,8 @@
             this.groupBox1.Controls.Add(this.MapSizeButton);
             this.groupBox1.Controls.Add(this.MapCol);
             this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("HP Simplified Hans", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.SaddleBrown;
             this.groupBox1.Location = new System.Drawing.Point(12, 88);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(200, 100);
@@ -112,6 +114,8 @@
             this.groupBox2.Controls.Add(this.GuessEnterButton);
             this.groupBox2.Controls.Add(this.ColumnGuess);
             this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Font = new System.Drawing.Font("HP Simplified Hans", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.SaddleBrown;
             this.groupBox2.Location = new System.Drawing.Point(12, 207);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(200, 100);
@@ -123,7 +127,7 @@
             // 
             this.RowGuess.Location = new System.Drawing.Point(6, 55);
             this.RowGuess.Name = "RowGuess";
-            this.RowGuess.Size = new System.Drawing.Size(34, 20);
+            this.RowGuess.Size = new System.Drawing.Size(34, 22);
             this.RowGuess.TabIndex = 3;
             // 
             // label3
@@ -131,15 +135,16 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(61, 27);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.Size = new System.Drawing.Size(61, 14);
             this.label3.TabIndex = 6;
             this.label3.Text = "Column #";
             // 
             // GuessEnterButton
             // 
+            this.GuessEnterButton.Font = new System.Drawing.Font("HP Simplified Hans", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.GuessEnterButton.Location = new System.Drawing.Point(124, 55);
             this.GuessEnterButton.Name = "GuessEnterButton";
-            this.GuessEnterButton.Size = new System.Drawing.Size(41, 23);
+            this.GuessEnterButton.Size = new System.Drawing.Size(57, 23);
             this.GuessEnterButton.TabIndex = 0;
             this.GuessEnterButton.Text = "Enter";
             this.GuessEnterButton.UseVisualStyleBackColor = true;
@@ -149,7 +154,7 @@
             // 
             this.ColumnGuess.Location = new System.Drawing.Point(64, 55);
             this.ColumnGuess.Name = "ColumnGuess";
-            this.ColumnGuess.Size = new System.Drawing.Size(44, 20);
+            this.ColumnGuess.Size = new System.Drawing.Size(44, 22);
             this.ColumnGuess.TabIndex = 5;
             // 
             // label4
@@ -157,17 +162,18 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(6, 27);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.Size = new System.Drawing.Size(42, 14);
             this.label4.TabIndex = 4;
             this.label4.Text = "Row #";
             // 
             // Title
             // 
             this.Title.AutoSize = true;
-            this.Title.Font = new System.Drawing.Font("Britannic Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Title.Location = new System.Drawing.Point(304, 24);
+            this.Title.Font = new System.Drawing.Font("HP Simplified", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.ForestGreen;
+            this.Title.Location = new System.Drawing.Point(266, 18);
             this.Title.Name = "Title";
-            this.Title.Size = new System.Drawing.Size(213, 23);
+            this.Title.Size = new System.Drawing.Size(262, 32);
             this.Title.TabIndex = 9;
             this.Title.Text = "Tropical Island Game!";
             this.Title.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -184,6 +190,8 @@
             // 
             // Instructions
             // 
+            this.Instructions.Font = new System.Drawing.Font("HP Simplified Hans", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Instructions.ForeColor = System.Drawing.Color.ForestGreen;
             this.Instructions.Location = new System.Drawing.Point(12, 36);
             this.Instructions.Name = "Instructions";
             this.Instructions.Size = new System.Drawing.Size(200, 35);
@@ -196,6 +204,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.Instructions);
             this.Controls.Add(this.pictureBox1);
